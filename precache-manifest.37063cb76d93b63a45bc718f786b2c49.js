07624395b19356c5d9fc91a0bf0f89bab179e375
Eric Tang <cs.erictang@gmail.com>
2019-03-31 23:24:00 -0700

    Updates

---
+++ precache-manifest.37063cb76d93b63a45bc718f786b2c49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b8b0192b0945f28cb2f",
+    "url": "/puzzle_pages/static/css/main.32e3b2f8.chunk.css"
+  },
+  {
+    "revision": "3b8b0192b0945f28cb2f",
+    "url": "/puzzle_pages/static/js/main.c9cb708b.chunk.js"
+  },
+  {
+    "revision": "74cf672ad3a907a62277",
+    "url": "/puzzle_pages/static/js/runtime~main.c99926d9.js"
+  },
+  {
+    "revision": "dd70478a1a54e4417401",
+    "url": "/puzzle_pages/static/js/2.5cd30901.chunk.js"
+  },
+  {
+    "revision": "789f3fce9343dd0dc1331a64bcd4cb9d",
+    "url": "/puzzle_pages/static/media/puzzle-splash-left.789f3fce.png"
+  },
+  {
+    "revision": "1f7a742a52dc9de92a7ba0c622b15175",
+    "url": "/puzzle_pages/static/media/puzzle-splash-right.1f7a742a.png"
+  },
+  {
+    "revision": "290c300bae8a49841f7031a773f1434e",
+    "url": "/puzzle_pages/index.html"
+  }
+];
